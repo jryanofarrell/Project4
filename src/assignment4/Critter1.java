a5cc6f173f16879750c49d018bd28d9922070d08
ClemsRocket <leclemsdu64@hotmail.fr>
2016-10-20 20:28:01 -0500

    Added headers

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,3 +1,15 @@
+/* CRITTERS Critter.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Clement PARDON
+ * cp34735
+ * 16460
+ * John OFarrell
+ * jro769
+ * 16455
+ * Slip days used: <0>
+ * Fall 2016
+ */
 package assignment4;
 
 public class Critter1 extends Critter {
--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -1,12 +1,12 @@
-/* CRITTERS Main.java
+/* CRITTERS Critter.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
- * <Student2 Name>
- * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * Clement PARDON
+ * cp34735
+ * 16460
+ * John OFarrell
+ * jro769
+ * 16455
  * Slip days used: <0>
  * Fall 2016
  */
